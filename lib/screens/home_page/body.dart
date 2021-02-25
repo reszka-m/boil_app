ae91929ad627b25e4031eff922c153f27ebc6423
reszka-m <reszkam19@gmail.com>
2021-02-25 10:23:52 +0100

    GridView1

--- lib/screens/home_page/body.dart
+++ lib/screens/home_page/body.dart
@@ -10,7 +10,103 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         SearchBox(size: size),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: GridView.count(
+            padding: EdgeInsets.only(
+                left: kDefaultPadding,
+                right: kDefaultPadding,
+                bottom: kDefaultPadding / 2,
+                top: kDefaultPadding / 2),
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            crossAxisCount: 2,
+            children: [
+              SingleTile(
+                image: "assets/images/img1.png",
+                name: "Rice",
+              ),
+              SingleTile(
+                image: "assets/images/img2.png",
+                name: "Pasta",
+              ),
+              SingleTile(
+                image: "assets/images/img3.png",
+                name: "Potatoes",
+              ),
+              SingleTile(
+                image: "assets/images/img1.png",
+                name: "Your stara",
+              ),
+            ],
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class SingleTile extends StatelessWidget {
+  const SingleTile({
+    Key key,
+    this.image,
+    this.name,
+    this.press,
+  }) : super(key: key);
+  final String image, name;
+  final Function press;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              width: size.width * 0.4,
+              height: size.height * 0.15,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                image: DecorationImage(
+                    image: AssetImage(image), fit: BoxFit.cover),
+              ),
+            ),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 20,
+                      color: kPrimaryColor.withOpacity(0.4)),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Spacer(),
+                  RichText(
+                    text: TextSpan(
+                        text: name.toUpperCase(),
+                        style: Theme.of(context).textTheme.button),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
