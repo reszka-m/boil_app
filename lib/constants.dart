de3c353be5be7adae85a1c6b3062810fc9fe1d22
reszka-m <reszkam19@gmail.com>
2021-03-03 16:44:57 +0100

    update 2137

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFFc4c4c4);
-const kSecondaryColor = Color(0xFF585858);
+const kPrimaryColor = Color(0xFF585858);
+const kSecondaryColor = Color(0xFFc4c4c4);
 const double kDefaultPadding = 20.0;
 const kTextColor = Color(0xFF555555);
