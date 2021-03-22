80fb93882d4e31d81e49a86baf24bd7ea5f4495e
reszka-m <reszkam19@gmail.com>
2021-03-22 14:19:28 +0100

    Test Ad Implemented

--- lib/screens/home_page/body.dart
+++ lib/screens/home_page/body.dart
@@ -1,10 +1,36 @@
+import 'package:boil_app/ad_state.dart';
 import 'package:boil_app/screens/home_page/components/upper_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
 import '../../constants.dart';
 import 'components/item_list_1.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  BannerAd banner;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final adState = Provider.of<AdState>(context);
+    adState.initialization.then((status) {
+      setState(() {
+        banner = BannerAd(
+          adUnitId: adState.bannerAdUnitId,
+          size: AdSize.banner,
+          request: AdRequest(),
+          listener: adState.adListener,
+        )..load();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -17,6 +43,13 @@ class Body extends StatelessWidget {
               height: 10,
             ),
             ItemList1(),
+            if (banner == null)
+              SizedBox(height: 50)
+            else
+              Container(
+                height: 50,
+                child: AdWidget(ad: banner),
+              )
           ],
         ),
       ],
