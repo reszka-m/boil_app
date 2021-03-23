89efd66a306f9305f46241e7d69a94bf2aa6f20b
reszka-m <reszkam19@gmail.com>
2021-03-23 12:57:15 +0100

    block rotation

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:boil_app/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   final initFuture = MobileAds.instance.initialize();
   final adState = AdState(initFuture);
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(
     Provider.value(
       value: adState,
